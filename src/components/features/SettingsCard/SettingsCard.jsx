0c137c9581b0c73ae9af8f556e47fcc2839091ef
Sarah BOURGEOIS <sarah.bourgeois.bc@gmail.com>
2025-03-25 23:24:15 +0100

    fix minor wordings

--- src/components/features/SettingsCard/SettingsCard.jsx
+++ src/components/features/SettingsCard/SettingsCard.jsx
@@ -11,17 +11,17 @@ function SettingsCard() {
 
   return (
     <div className="bg-gray-900 rounded-2xl shadow-lg p-6 space-y-6  flex flex-col  h-full">
-      <h2 className="text-2xl font-semibold text-blue-400">QR Code Parameter</h2>
+      <h2 className="text-2xl font-semibold text-blue-400">QR Code Parameters</h2>
       
       <TextInput 
-        label={"Enter url"} 
+        label={"Enter text or url"} 
         placeholder={"https://example.com"} 
         value={text}  
         onChange={e => setText(e.target.value)}
         />
      
       <RangeSlider 
-        label="Taille : " 
+        label="Size : " 
         min={100} 
         max={1000} 
         value={size} 
@@ -31,7 +31,7 @@ function SettingsCard() {
       <div className="grid grid-cols-2 gap-4">
         <ColorPicker label="Background Color" value={bgColor} onChange={e => setBgColor(e.target.value)}/>
 
-        <ColorPicker label="Color"  value={fgColor} onChange={e => setFgColor(e.target.value)}/>
+        <ColorPicker label="Foreground Color"  value={fgColor} onChange={e => setFgColor(e.target.value)}/>
       </div>
 
     </div>
